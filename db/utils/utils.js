082879dda39b8c1388b47562d6bd6f587ebe28de
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 12:53:05 +0100

    passing 3rd test on format comments

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -15,4 +15,14 @@ exports.makeRefObj = (list) => {
 	return refObj;
 };
 
-exports.formatComments = (comments, articleRef) => {};
+exports.formatComments = (comments, articleRef) => {
+	if (!comments.length || !Object.keys(articleRef).length) return [];
+	const { created_by, belongs_to, ...restOfCommentData } = comments[0];
+	if (created_by) {
+		restOfCommentData.author = created_by;
+	}
+	if (belongs_to) {
+		restOfCommentData.article_id = articleRef[belongs_to];
+	}
+	return [ restOfCommentData ];
+};
