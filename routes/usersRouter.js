64cda192243baf0dad8fc1f53b53a7f08d95b510
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 22:43:49 +0100

    added 405 error handling on users route

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,6 +1,7 @@
 const usersRouter = require('express').Router();
 const { sendGetUserByUsername } = require('../controller/users-controller.js');
+const { sendMethodNotAllowed } = require('../errors/error.js');
 
-usersRouter.route('/:username').get(sendGetUserByUsername);
+usersRouter.route('/:username').get(sendGetUserByUsername).all(sendMethodNotAllowed);
 
 module.exports = usersRouter;
