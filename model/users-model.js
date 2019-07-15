b2e5c8b5943302a92888259b7922448855fdcd8e
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 16:41:27 +0100

    test passing users route

--- model/users-model.js
+++ model/users-model.js
@@ -1,7 +1,23 @@
 const connection = require('../db/connection.js');
 
 const getUserByUsername = ({ username }) => {
-	return connection.first('*').from('users').where('username', username);
+	const reg = /([A-Z])\w+/i;
+
+	if (!reg.test(username)) {
+		return Promise.reject({
+			status: 400,
+			msg: 'Bad Request'
+		});
+	}
+	return connection.first('*').from('users').where('username', username).then((user) => {
+		if (!user) {
+			return Promise.reject({
+				status: 404,
+				msg: 'User Not Found'
+			});
+		}
+		return user;
+	});
 };
 
 module.exports = { getUserByUsername };
