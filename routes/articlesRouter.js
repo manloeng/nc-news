f9c175e7ac5085c6c6ba33a91dc06392d9fc283e
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 22:46:07 +0100

    added error handling on articles router

--- routes/articlesRouter.js
+++ routes/articlesRouter.js
@@ -1,6 +1,7 @@
 const articlesRouter = require('express').Router();
 const { sendGetArticleById, sendPatchArticleById } = require('../controller/articles-controller.js');
+const { sendMethodNotAllowed } = require('../errors/error.js');
 
-articlesRouter.route('/:article_id').get(sendGetArticleById).patch(sendPatchArticleById);
+articlesRouter.route('/:article_id').get(sendGetArticleById).patch(sendPatchArticleById).all(sendMethodNotAllowed);
 
 module.exports = articlesRouter;
