00f15880045753ba5a8ae70cd45f8431c2af439d
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 11:58:22 +0100

    created articles migration

---
+++ db/migrations/20190715105703_articles.js
@@ -0,0 +1,12 @@
+exports.up = function(knex) {
+	return knex.schema.createTable('articles', (articlesTable) => {
+		articlesTable.increments('article_id').primary();
+		articlesTable.string('title');
+		articlesTable.integer('topic').references('topics.slug');
+		articlesTable.string('author').references('users.username');
+		articlesTable.text('body');
+		articlesTable.timestamp('created_at').defaultTo(knex.fn.now());
+	});
+};
+
+exports.down = function(knex) {};
