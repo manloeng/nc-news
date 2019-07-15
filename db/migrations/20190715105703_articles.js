69744d91d36dd2efce727ec6049e6637f6254654
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 14:36:20 +0100

    amended data format in migration tables

--- db/migrations/20190715105703_articles.js
+++ db/migrations/20190715105703_articles.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
 	return knex.schema.createTable('articles', (articlesTable) => {
 		articlesTable.increments('article_id').primary();
 		articlesTable.string('title').notNullable();
-		articlesTable.integer('topic').references('topics.slug');
+		articlesTable.string('topic').references('topics.slug');
 		articlesTable.string('author').references('users.username');
 		articlesTable.text('body');
 		articlesTable.timestamp('created_at').defaultTo(knex.fn.now());
--- db/migrations/20190715105709_comments.js
+++ db/migrations/20190715105709_comments.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
 	return knex.schema.createTable('comments', (commentsTable) => {
 		commentsTable.increments('comment_id').primary();
 		commentsTable.text('body');
-		commentsTable.string('article_id').references('articles.article_id');
+		commentsTable.integer('article_id').references('articles.article_id');
 		commentsTable.string('author').references('users.username');
 		commentsTable.integer('votes');
 		commentsTable.timestamp('created_at').defaultTo(knex.fn.now());
