4bb37506800b017fe00bfb99676bfd4d67a55581
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 14:36:45 +0100

    date is being formatted and passed in seed

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -15,27 +15,22 @@ exports.seed = function(knex) {
 			return Promise.all([ topicsInsertions, usersInsertions ]);
 		})
 		.then(() => {
+			const formmatedArticles = formatDates(articleData);
+			return knex.insert(formmatedArticles).into('articles').returning('*');
+		})
+		.then((articleRows) => {
+			console.log(articleRows);
 			/*
 
-	    Your article data is currently in the incorrect format and will violate your SQL schema.
+	  Your comment data is currently in the incorrect format and will violate your SQL schema.
 
-	    You will need to write and test the provided formatDate utility function to be able insert your article data.
+	  Keys need renaming, values need changing, and most annoyingly, your comments currently only refer to the title of the article they belong to, not the id.
 
-	    Your comment insertions will depend on information from the seeded articles, so make sure to return the data after it's been seeded.
-	    */
+	  You will need to write and test the provided makeRefObj and formatComments utility functions to be able insert your comment data.
+	  */
+
+			const articleRef = makeRefObj(articleRows);
+			const formattedComments = formatComments(commentData, articleRef);
+			return knex('comments').insert(formattedComments);
 		});
-	//   .then(articleRows => {
-	//     /*
-
-	//     Your comment data is currently in the incorrect format and will violate your SQL schema.
-
-	//     Keys need renaming, values need changing, and most annoyingly, your comments currently only refer to the title of the article they belong to, not the id.
-
-	//     You will need to write and test the provided makeRefObj and formatComments utility functions to be able insert your comment data.
-	//     */
-
-	//     const articleRef = makeRefObj(articleRows);
-	//     const formattedComments = formatComments(commentData, articleRef);
-	//     return knex('comments').insert(formattedComments);
-	//   });
 };
