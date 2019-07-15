c40074fcaa612332956e1a1e30f89d1d8fc5085e
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 12:11:58 +0100

    amended test to give correct output

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -10,12 +10,17 @@ describe('formatDates', () => {
 	it('returns empty array when passed with an empty array', () => {
 		const input = [ { created_at: 0 } ];
 		const expected = formatDates(input);
-		expect(expected).to.eql([ new Date(0) ]);
+		expect(expected).to.eql([ { created_at: new Date(0) } ]);
 	});
 	it('returns empty array when passed with an empty array', () => {
 		const input = [ { created_at: 1563188900 } ];
 		const expected = formatDates(input);
-		expect(expected).to.eql([ new Date(1563188900) ]);
+		expect(expected).to.eql([ { created_at: new Date(1563188900) } ]);
+	});
+	it('returns empty array when passed with an empty array', () => {
+		const input = [ { created_at: 0 }, { created_at: 1563188900 } ];
+		const expected = formatDates(input);
+		expect(expected).to.eql([ { created_at: new Date(0) }, { created_at: new Date(1563188900) } ]);
 	});
 });
 
