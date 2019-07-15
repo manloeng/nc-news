4f488fff4753662e5cc3ba18cde62cd7c50d40f0
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 15:01:45 +0100

    removed unnecessary if statements

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -20,15 +20,9 @@ exports.formatComments = (comments, articleRef) => {
 	if (!comments.length || !Object.keys(articleRef).length) return [];
 
 	const mapArr = comments.map(({ created_by, belongs_to, created_at, ...restOfCommentData }) => {
-		if (created_by) {
-			restOfCommentData.author = created_by;
-		}
-		if (belongs_to) {
-			restOfCommentData.article_id = articleRef[belongs_to];
-		}
-		if (created_at >= 0) {
-			restOfCommentData.created_at = new Date(created_at);
-		}
+		restOfCommentData.author = created_by;
+		restOfCommentData.article_id = articleRef[belongs_to];
+		restOfCommentData.created_at = new Date(created_at);
 		return restOfCommentData;
 	});
 	return mapArr;
