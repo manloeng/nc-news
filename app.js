dcaf60281192842513a480d48e18284c0e2fc99a
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 15:32:37 +0100

    Router middleware is created

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const apiRouter = require('./routes/apiRouter.js');
+
+app.use('/api', apiRouter);
 
 app.all('/*', (req, res, next) => {
 	res.status(404).send({ msg: 'Page Not Found' });
