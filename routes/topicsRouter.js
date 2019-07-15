2994f97312f9a80585d4cbc120a28a2b01201a1c
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 22:39:16 +0100

    handling error 405 on topics

--- routes/topicsRouter.js
+++ routes/topicsRouter.js
@@ -1,6 +1,7 @@
 const topicsRouter = require('express').Router();
 const { sendGetTopics } = require('../controller/topics-controller.js');
+const { sendMethodNotAllowed } = require('../errors/error.js');
 
-topicsRouter.route('/').get(sendGetTopics);
+topicsRouter.route('/').get(sendGetTopics).all(sendMethodNotAllowed);
 
 module.exports = topicsRouter;
