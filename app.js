92f711c097cbf27d232485830ff371e3cc51b5c7
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 16:41:14 +0100

    added error handling for invalid request

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ app.all('/*', (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-	console.log(err);
+	// console.log(err);
+	if (err.status) {
+		res.status(err.status).send({ msg: err.msg });
+	}
 });
 
 module.exports = app;
