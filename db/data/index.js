ac162242abc253fa20e4c82d7bfcf213d04c6a05
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 10:45:44 +0100

    connected test and dev data to index.js

---
+++ db/data/index.js
@@ -0,0 +1,11 @@
+const ENV = process.env.NODE_ENV || 'development';
+
+const devData = require('./development-data');
+const testData = require('./test-data');
+
+const data = {
+	test: testData,
+	development: devData
+};
+
+module.exports = data[ENV];
