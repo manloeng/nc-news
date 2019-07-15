34365218df496ed6e9855c57969aa7be60a68c2e
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 22:50:47 +0100

    refactored for error handling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routes/apiRouter.js');
+const { customError, sqlErrors, serverErrors } = require('./errors/error.js');
 
 app.use(express.json());
 app.use('/api', apiRouter);
@@ -9,15 +10,6 @@ app.all('/*', (req, res, next) => {
 	res.status(404).send({ msg: 'Page Not Found' });
 });
 
-app.use((err, req, res, next) => {
-	// console.log(err.message);
-	if (err.status) {
-		res.status(err.status).send({ msg: err.msg });
-	}
-	const reg = /invalid.+/g;
-	if (err.code === '22P02') {
-		res.status(400).send({ msg: err.message.match(reg)[0] });
-	}
-});
+app.use(customError, sqlErrors, serverErrors);
 
 module.exports = app;
