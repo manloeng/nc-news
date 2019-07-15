9bec506dac64ff6712bf67fd38eb8089573be1b2
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 12:30:39 +0100

    passing 2nd utils test

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -7,7 +7,14 @@ exports.formatDates = (list) => {
 };
 
 exports.makeRefObj = (list) => {
-	return {};
+	if (!list.length) return {};
+	console.log(list);
+	const refObj = {};
+	const { article_id, title } = list[0];
+	console.log(article_id);
+	console.log(title);
+	refObj[title] = article_id;
+	return refObj;
 };
 
 exports.formatComments = (comments, articleRef) => {};
