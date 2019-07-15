795602358729af7ed08220a300049115427495e1
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 13:05:44 +0100

    test passing 4th test -formatComment

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -17,12 +17,18 @@ exports.makeRefObj = (list) => {
 
 exports.formatComments = (comments, articleRef) => {
 	if (!comments.length || !Object.keys(articleRef).length) return [];
-	const { created_by, belongs_to, ...restOfCommentData } = comments[0];
-	if (created_by) {
-		restOfCommentData.author = created_by;
-	}
-	if (belongs_to) {
-		restOfCommentData.article_id = articleRef[belongs_to];
-	}
-	return [ restOfCommentData ];
+
+	const mapArr = comments.map(({ created_by, belongs_to, created_at, ...restOfCommentData }) => {
+		if (created_by) {
+			restOfCommentData.author = created_by;
+		}
+		if (belongs_to) {
+			restOfCommentData.article_id = articleRef[belongs_to];
+		}
+		if (created_at >= 0) {
+			restOfCommentData.created_at = new Date(created_at);
+		}
+		return restOfCommentData;
+	});
+	return mapArr;
 };
