73c45fb1aa8f8432ed359b16dae41c38a79b8e4b
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 11:58:45 +0100

    users and topics insertions are functional

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -2,22 +2,28 @@ const { topicData, articleData, commentData, userData } = require('../data/index
 
 const { formatDates, formatComments, makeRefObj } = require('../utils/utils');
 
-exports.seed = function(knex, Promise) {
-	const topicsInsertions = knex('topics').insert(topicData);
-	const usersInsertions = knex('users').insert(userData);
+exports.seed = function(knex) {
+	const topicsInsertions = knex('topics').insert(topicData).returning('*');
+	const usersInsertions = knex('users').insert(userData).returning('*');
 
-	return topicsInsertions;
-	// return Promise.all([topicsInsertions, usersInsertions])
-	//   .then(() => {
-	//     /*
+	return knex.migrate
+		.rollback()
+		.then(() => {
+			return knex.migrate.latest();
+		})
+		.then(() => {
+			return Promise.all([ topicsInsertions, usersInsertions ]);
+		})
+		.then(() => {
+			/*
 
-	//     Your article data is currently in the incorrect format and will violate your SQL schema.
+	    Your article data is currently in the incorrect format and will violate your SQL schema.
 
-	//     You will need to write and test the provided formatDate utility function to be able insert your article data.
+	    You will need to write and test the provided formatDate utility function to be able insert your article data.
 
-	//     Your comment insertions will depend on information from the seeded articles, so make sure to return the data after it's been seeded.
-	//     */
-	//   })
+	    Your comment insertions will depend on information from the seeded articles, so make sure to return the data after it's been seeded.
+	    */
+		});
 	//   .then(articleRows => {
 	//     /*
 
