3a465015e4917727a694a2efc34733ffde75f83e
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 18:03:10 +0100

    error handling for article that hasn't been found

--- model/articles-model.js
+++ model/articles-model.js
@@ -1,9 +1,15 @@
 const connection = require('../db/connection.js');
 
 const getArticleById = ({ article_id }) => {
-	console.log(article_id);
-
-	return connection.first('*').from('articles').where('article_id', article_id);
+	return connection.first('*').from('articles').where('article_id', article_id).then((article) => {
+		if (!article) {
+			return Promise.reject({
+				status: 404,
+				msg: 'Article Not Found'
+			});
+		}
+		return article;
+	});
 };
 
 module.exports = { getArticleById };
