51e941723141a82b42e442a8f278753c9938c3ca
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 14:42:24 +0100

    seed is in fully functional

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -3,8 +3,8 @@ const { topicData, articleData, commentData, userData } = require('../data/index
 const { formatDates, formatComments, makeRefObj } = require('../utils/utils');
 
 exports.seed = function(knex) {
-	const topicsInsertions = knex('topics').insert(topicData).returning('*');
-	const usersInsertions = knex('users').insert(userData).returning('*');
+	const topicsInsertions = knex('topics').insert(topicData);
+	const usersInsertions = knex('users').insert(userData);
 
 	return knex.migrate
 		.rollback()
@@ -19,16 +19,6 @@ exports.seed = function(knex) {
 			return knex.insert(formmatedArticles).into('articles').returning('*');
 		})
 		.then((articleRows) => {
-			console.log(articleRows);
-			/*
-
-	  Your comment data is currently in the incorrect format and will violate your SQL schema.
-
-	  Keys need renaming, values need changing, and most annoyingly, your comments currently only refer to the title of the article they belong to, not the id.
-
-	  You will need to write and test the provided makeRefObj and formatComments utility functions to be able insert your comment data.
-	  */
-
 			const articleRef = makeRefObj(articleRows);
 			const formattedComments = formatComments(commentData, articleRef);
 			return knex('comments').insert(formattedComments);
