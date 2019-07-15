274fbf4bf96aacc9dd9219f6abc11007684a6fae
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 22:50:35 +0100

    created error handling middleware file

--- errors/error.js
+++ errors/error.js
@@ -2,4 +2,22 @@ const sendMethodNotAllowed = (req, res, next) => {
 	res.status(405).send({ msg: 'Method Not Allowed' });
 };
 
-module.exports = { sendMethodNotAllowed };
+const customError = (err, req, res, next) => {
+	if (err.status) {
+		res.status(err.status).send({ msg: err.msg });
+	} else next(err);
+};
+
+const sqlErrors = (err, req, res, next) => {
+	const reg = /invalid.+/g;
+	if (err.code === '22P02') {
+		res.status(400).send({ msg: err.message.match(reg)[0] });
+	} else next(err);
+};
+
+const serverErrors = (err, req, res, next) => {
+	console.log(err);
+	res.status(500).send({ msg: 'Internal Server Error' });
+};
+
+module.exports = { sendMethodNotAllowed, customError, sqlErrors, serverErrors };
