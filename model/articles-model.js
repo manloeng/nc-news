8025957baf736a7d72f25e23f01d0282568e0504
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 17:57:44 +0100

    removing console.log

--- model/articles-model.js
+++ model/articles-model.js
@@ -1,6 +1,8 @@
 const connection = require('../db/connection.js');
 
 const getArticleById = ({ article_id }) => {
+	console.log(article_id);
+
 	return connection.first('*').from('articles').where('article_id', article_id);
 };
 
