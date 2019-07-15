fd7840ad6eb926d22908cf8b6b052bcb773f281b
Andrew <manloengchung@yahoo.co.uk>
2019-07-15 10:45:00 +0100

    linked dev data to index.js

---
+++ db/data/development-data/index.js
@@ -0,0 +1,6 @@
+const articlesData = require('./articles.js');
+const commentsData = require('./comments.js');
+const topicsData = require('./topics.js');
+const usersData = require('./users.js');
+
+module.exports = { articlesData, commentsData, topicsData, usersData };
