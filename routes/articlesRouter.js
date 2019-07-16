d76bcde3ed99b71a75c4e4bde1647ec524da52f5
Andrew <manloengchung@yahoo.co.uk>
2019-07-16 14:23:12 +0100

    error handling for 405 - connected

--- routes/articlesRouter.js
+++ routes/articlesRouter.js
@@ -4,6 +4,10 @@ const { sendPostCommentByArticleId, sendGetCommentByArticleId } = require('../co
 const { sendMethodNotAllowed } = require('../errors/error.js');
 
 articlesRouter.route('/:article_id').get(sendGetArticleById).patch(sendPatchArticleById).all(sendMethodNotAllowed);
-articlesRouter.route('/:article_id/comments').post(sendPostCommentByArticleId).get(sendGetCommentByArticleId);
+articlesRouter
+	.route('/:article_id/comments')
+	.post(sendPostCommentByArticleId)
+	.get(sendGetCommentByArticleId)
+	.all(sendMethodNotAllowed);
 
 module.exports = articlesRouter;
