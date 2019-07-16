96511bd5036ded68277eb3d79ca08799920278dc
Andrew <manloengchung@yahoo.co.uk>
2019-07-16 11:24:39 +0100

    added error handling when no username input

--- model/comments-model.js
+++ model/comments-model.js
@@ -1,18 +1,17 @@
 const connection = require('../db/connection.js');
 
 const postCommentByArticleId = ({ article_id }, { username, body, ...restOftheBody }) => {
-	if (!username || !body) {
-		return Promise.reject({
-			status: 400,
-			msg: 'Require Input'
-		});
-	}
 	const formattedObj = {};
 	formattedObj.article_id = article_id;
 	formattedObj.author = username;
 	formattedObj.body = body;
 	return connection.insert(formattedObj).into('comments').returning('*').then((comment) => {
-		return comment[0];
+		if (comment[0].author === null) {
+			return Promise.reject({
+				status: 400,
+				msg: 'Require Username Input'
+			});
+		} else return comment[0];
 	});
 };
 
