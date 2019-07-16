853cc4b74c023fda264eabe2d76d69c24683a14f
Andrew <manloengchung@yahoo.co.uk>
2019-07-16 14:29:59 +0100

    connecting comments route

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2,9 +2,11 @@ const apiRouter = require('express').Router();
 const topicsRouter = require('./topicsRouter.js');
 const usersRouter = require('./usersRouter.js');
 const articlesRouter = require('./articlesRouter.js');
+const commentsRouter = require('./commentsRouter.js');
 
 apiRouter.use('/topics', topicsRouter);
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/articles', articlesRouter);
+apiRouter.use('/comments', commentsRouter);
 
 module.exports = apiRouter;
