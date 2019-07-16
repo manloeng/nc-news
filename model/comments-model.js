eb8593457634caba0d6f38b681b3193c599368c8
Andrew <manloengchung@yahoo.co.uk>
2019-07-16 10:51:59 +0100

    refactored code for test

--- model/comments-model.js
+++ model/comments-model.js
@@ -1,8 +1,13 @@
 const connection = require('../db/connection.js');
 
-const postCommentByArticleId = ({ article_id }, { ...body }) => {
-	body.article_id = article_id;
-	return connection.insert(body).into('comments').returning('*').then((comment) => comment[0]);
+const postCommentByArticleId = ({ article_id }, { username, body, ...restOftheBody }) => {
+	const formattedObj = {};
+	formattedObj.article_id = article_id;
+	formattedObj.author = username;
+	formattedObj.body = body;
+	return connection.insert(formattedObj).into('comments').returning('*').then((comment) => {
+		return comment[0];
+	});
 };
 
 module.exports = { postCommentByArticleId };
