51196b6ce850194c91d6daffa36cfca76404b2a3
Andrew <manloengchung@yahoo.co.uk>
2019-07-17 10:21:36 +0100

    refactored errors

--- errors/error.js
+++ errors/error.js
@@ -3,37 +3,26 @@ const sendMethodNotAllowed = (req, res, next) => {
 };
 
 const customError = (err, req, res, next) => {
-	// console.log(err);
 	if (err.status) {
 		res.status(err.status).send({ msg: err.msg });
 	} else next(err);
 };
 
 const sqlErrors = (err, req, res, next) => {
-	// const sqlErrCodes = {
-	// 	'22P02': 400
-	// };
+	const sqlErrCodes = {
+		'22P02': /invalid.+/g,
+		'23502': /null.+/g,
+		'23503': /insert or.+/g,
+		'42703': /column.+/g
+	};
 
-	if (err.code === '22P02') {
-		const reg = /invalid.+/g;
-		res.status(400).send({ msg: err.message.match(reg)[0] });
-	}
-	if (err.code === '23502') {
-		const reg = /null.+/g;
-		res.status(400).send({ msg: err.message.match(reg)[0] });
-	}
-	if (err.code === '23503') {
-		const reg = /insert or.+/g;
-		res.status(400).send({ msg: err.message.match(reg)[0] });
-	}
-	if (err.code === '42703') {
-		const reg = /column.+/g;
-		res.status(400).send({ msg: err.message.match(reg)[0] });
+	if (err.code in sqlErrCodes) {
+		const errorMsg = err.message.match(sqlErrCodes[err.code])[0];
+		res.status(400).send({ msg: errorMsg });
 	} else next(err);
 };
 
 const serverErrors = (err, req, res, next) => {
-	// console.log(err);
 	res.status(500).send({ msg: 'Internal Server Error' });
 };
 
