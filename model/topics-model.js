b8b4884f86834312b28b8b4bbc19e8b859917fc7
Andrew <manloengchung@yahoo.co.uk>
2019-07-18 20:22:58 +0100

    added post req

--- model/topics-model.js
+++ model/topics-model.js
@@ -4,8 +4,22 @@ const getTopics = () => {
 	return connection.select('*').from('topics');
 };
 
-const insertTopics = (reqBody) => {
-	return connection.insert(reqBody).into('topics').returning('*').then((topic) => topic[0]);
+const insertTopics = ({ slug, description, ...restOfReqBody }) => {
+	const reg = /([A-Z])\w+/i;
+	if (
+		// Checks for extra invalid queries
+		Object.keys(restOfReqBody).length > 0 ||
+		// Checks for invalid slug format (i.e Int)
+		!reg.test(slug)
+	) {
+		return Promise.reject({
+			status: 400,
+			msg: 'Bad Request'
+		});
+	}
+
+	const formattedBody = { slug, description };
+	return connection.insert(formattedBody).into('topics').returning('*').then((topic) => topic[0]);
 };
 
 module.exports = { getTopics, insertTopics };
