44271a336f00bbd3a78217be2e44a4968b0900bb
Andrew <manloengchung@yahoo.co.uk>
2019-07-18 15:50:14 +0100

    made ref not nullable

--- db/migrations/20190715105703_articles.js
+++ db/migrations/20190715105703_articles.js
@@ -2,8 +2,8 @@ exports.up = function(knex) {
 	return knex.schema.createTable('articles', (articlesTable) => {
 		articlesTable.increments('article_id').primary();
 		articlesTable.string('title').notNullable();
-		articlesTable.string('topic').references('topics.slug');
-		articlesTable.string('author').references('users.username');
+		articlesTable.string('topic').references('topics.slug').notNullable();
+		articlesTable.string('author').references('users.username').notNullable();
 		articlesTable.text('body');
 		articlesTable.integer('votes').defaultTo(0);
 		articlesTable.timestamp('created_at').defaultTo(knex.fn.now());
