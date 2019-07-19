8c04b567315512f382a2408e78085d499f2a2ad2
Andrew <manloengchung@yahoo.co.uk>
2019-07-19 10:26:53 +0100

    added onDelete cascade when deleting article id

--- db/migrations/20190715105709_comments.js
+++ db/migrations/20190715105709_comments.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
 	return knex.schema.createTable('comments', (commentsTable) => {
 		commentsTable.increments('comment_id').primary();
 		commentsTable.text('body').notNullable();
-		commentsTable.integer('article_id').references('articles.article_id');
+		commentsTable.integer('article_id').references('articles.article_id').onDelete('CASCADE');
 		commentsTable.string('author').references('users.username').notNullable();
 		commentsTable.integer('votes').defaultTo(0);
 		commentsTable.timestamp('created_at').defaultTo(knex.fn.now());
