988f23054957b349e80272cfaa30654fc4b8bd02
Andrew <manloengchung@yahoo.co.uk>
2019-07-19 13:01:01 +0100

    removed comments

--- model/login-model.js
+++ model/login-model.js
@@ -8,18 +8,6 @@ const insertLogin = ({ username, password, ...restOfReqBody }) => {
 		const token = jwt.sign({ user: user.username, iat: Date.now() }, JWT_SECRET);
 		return { token, user: user[0] };
 	});
-	// return connection.select('*').from('users').where('username', '=', username).then(([ user ]) => {
-	// 	console.log(user);
-	// 	if (!user || password !== user.password)
-	// 		return Promise.reject({ status: 401, msg: 'invalid username or password' });
-	// 	else {
-	// 		    const token = jwt.sign(
-	//     { user: user.username, iat: Date.now() },
-	//     JWT_SECRET
-	// );
-	// res.send({ token });
-	// 	}
-	// });
 };
 
 module.exports = { insertLogin };
