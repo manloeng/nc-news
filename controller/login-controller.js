4636b952460a4ab7b8ffb6f30a40fd33ad978e3e
Andrew <manloengchung@yahoo.co.uk>
2019-07-20 22:02:59 +0100

    added extra comments

--- controller/login-controller.js
+++ controller/login-controller.js
@@ -1,6 +1,7 @@
 const { insertLogin } = require('../model/login-model.js');
 
 const postLogin = (req, res, next) => {
+	//may need to add extra for login
 	insertLogin(req.body)
 		.then(({ ...passedObj }) => {
 			res.status(200).send({ ...passedObj });
