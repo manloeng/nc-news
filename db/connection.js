0dcfba757420394f619b9030fa9178dbbc5057c3
Andrew <manloengchung@yahoo.co.uk>
2019-07-21 16:02:28 +0100

    removed extra lines

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,4 @@
 const ENV = process.env.NODE_ENV || 'development';
-
 const knex = require('knex');
 const dbConfig = ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } : require('../knexfile');
 
